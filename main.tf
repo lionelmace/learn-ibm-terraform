38a66e2b862ae807bdb62872b1792aa1580425fa
Lionel Mace <lionel.mace@gmail.com>
2023-06-14 16:57:45 +0200

    clean up version

--- main.tf
+++ main.tf
@@ -31,45 +31,29 @@ resource "ibm_is_subnet" "subnet" {
   tags            = var.tags
 }
 
-# Create a VSI
 data "ibm_is_image" "image" {
   name = var.image_name
 }
 
-# ssh key to inject into the vsi and the instance
-# data "ibm_is_ssh_key" "sshkey" {
-#   name = var.ssh_key_name
-# }
-# resource "ibm_is_ssh_key" "sshkey" {
-#   name           = format("%s-%s", var.prefix, "ssh-key")
-#   resource_group = ibm_resource_group.rg.id
-#   public_key     = var.ssh_public_key
-# }
-
-# Trying to use code below to avoid passing the key as argument
-# Error: [DEBUG] Create SSH Key ssh: short read
-# BEGIN
 resource "tls_private_key" "example" {
-  # count     = var.ssh_key_id == null ? 1 : 0
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
+# Generate an SSH Key
 resource "ibm_is_ssh_key" "generated_key" {
-  # count          = var.ssh_key_id == null ? 1 : 0
   name           = "${local.basename}-ssh-key"
   resource_group = ibm_resource_group.rg.id
   public_key     = tls_private_key.example.public_key_openssh
 }
-# END
 
+# Create a VSI (Virtual Server Instance)
 resource "ibm_is_instance" "instance" {
   name           = "${local.basename}-vsi"
   vpc            = ibm_is_vpc.vpc.id
   zone           = ibm_is_subnet.subnet.zone
   profile        = var.profile_name
   image          = data.ibm_is_image.image.id
-  # keys           = [data.ibm_is_ssh_key.sshkey.id]
   keys           = [ibm_is_ssh_key.generated_key.id]
   resource_group = ibm_resource_group.rg.id
   tags           = var.tags
