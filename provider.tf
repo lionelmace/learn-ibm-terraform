a8cefa50125c03ad399adb74dcaa85fb156205ef
Lionel Mace <lionel.mace@gmail.com>
2023-10-10 14:38:45 +0200

    upgrade ibm cloud tf provider

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.56.2"
+      version = "1.58.1"
     }
     tls = {
       source  = "hashicorp/tls"
